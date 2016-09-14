c98c6fbf420f093ef4dc3ecbfb2e33b958c568de
Jacopo Soldani <jsoldani@gmail.com>
2016-09-14 17:41:54 +0200

    visualiser migrated to react, working

--- Barrel-visualiser.js
+++ Barrel-visualiser.js
@@ -15,52 +15,175 @@ var TopologyRow = React.createClass({
 })
 
 var TopologyTable = React.createClass({
-  getInitialState: function() { return { uiData: null } },
-  setUIData: function(uiData) {
-    this.setState({ uiData: uiData })
-  },
   render: function() {
-    if (this.state.uiData) {
-      console.log(this.state.uiData);
-  		// Setting the stage
-      var getUIName = id => this.state.uiData.uiNames[id] || id;
+    if (!this.props.uiData) return null;
+    // Setting the stage
+    var getUIName = id => this.props.uiData.uiNames[id] || id;
 
-  		var nodes = this.state.uiData.data.nodes;
-  		var nodeIds = Object.keys(nodes);
+  	var nodes = this.props.uiData.data.nodes;
+  	var nodeIds = Object.keys(nodes);
 
-  		// Rendering
-  		return (
-        <table className="table table-striped table-hover">
-          <thead>
-              <tr className="btn-primary disabled">
-                  <th>Node</th>
-                  <th>Node type</th>
-                  <th>Capabilities</th>
-                  <th>Requirements</th>
-                  <th>Operations</th>
-              </tr>
-          </thead>
-          <tbody> {
-            nodeIds.map(id => {
-              return (
-                <TopologyRow
-                  key={id}
-                  getUIName={getUIName}
-                  name={getUIName(id)}
-                  type={nodes[id].type}
-                  caps={nodes[id].caps}
-                  reqs={nodes[id].reqs}
-                  ops={nodes[id].ops} />
-              )
-            })
-        	}
-          </tbody>
-      </table>
-    );
-  }
-  else
-    return null;
+  	// Rendering
+  	return (
+      <table className="table table-striped table-hover">
+        <thead>
+            <tr className="btn-primary disabled">
+                <th>Node</th>
+                <th>Node type</th>
+                <th>Capabilities</th>
+                <th>Requirements</th>
+                <th>Operations</th>
+            </tr>
+        </thead>
+        <tbody> {
+          nodeIds.map(id => {
+            return (
+              <TopologyRow
+                key={id}
+                getUIName={getUIName}
+                name={getUIName(id)}
+                type={nodes[id].type}
+                caps={nodes[id].caps}
+                reqs={nodes[id].reqs}
+                ops={nodes[id].ops} />
+            )
+          })
+      	}
+        </tbody>
+    </table>
+  );
  }
 });
 
-nodeTable = ReactDOM.render(<TopologyTable />, document.getElementById('topology-table'));
+var Visualiser = React.createClass({
+  getInitialState: function () {
+    return {
+      appName: null,
+      uiData: null
+    }
+  },
+  update: function(appName,uiData) {
+    this.setState({
+      appName: appName,
+      uiData: uiData
+    })
+  },
+  render: function() {
+    if(!this.state.uiData) return null;
+    return (
+      <div>
+        <h1 className="legend" style={{textDecoration:"bold"}}>
+          {this.state.appName}
+        </h1>
+        <h4>Application topology</h4>
+        <table>
+          <tr>
+            <td style={{width:"40%"}}>
+              <div id="app-topology" style={{position: "relative"}}></div>
+            </td>
+            <td style={{width:"60%"}}>
+              <div style={{fontSize:"90%",marginLeft:"10px"}}>
+                <TopologyTable uiData={this.state.uiData} />
+              </div>
+            </td>
+          </tr>
+        </table>
+      </div>
+    );
+  },
+  componentDidUpdate: function () {
+    var drawTopology = function(parentDiv, topology) {
+      // Parse the application topology
+      var nodes = topology.data.nodes;
+      var cells = {};
+
+      var toUI = function (s) { return topology.uiNames[s] || s; };
+
+      var TopologyNode = joint.shapes.basic.Generic.extend(_.extend({}, joint.shapes.basic.PortsModelInterface, {
+        markup: '<g class="rotatable"><g class="scalable"><rect class="body"/></g><text class="label"/><g class="inPorts"/><g class="outPorts"/></g>',
+        portMarkup: '<g class="port port<%= id %>"><circle class="port-body"/><text class="port-label"/></g>',
+        defaults: joint.util.deepSupplement({
+          type: 'devs.Model',
+          size: { width: 1, height: 1 },
+          inPorts: [],
+          outPorts: [],
+          attrs: {
+            '.': { magnet: false },
+            '.body': { width: 150, height: 50, 'rx': 10, 'ry': 5 },
+            '.port-body': { r: 8, magnet: 'passive' },
+            text: { 'pointer-events': 'none' },
+            '.label': { text: 'Model', 'ref-x': .5, 'ref-y': 18, ref: '.body', 'text-anchor': 'middle' },
+            '.inPorts .port-label': { x: 4, y: -9, 'text-anchor': 'start', transform: 'rotate(330)' },
+            '.outPorts .port-label': { x: 1, y: 19, 'text-anchor': 'end', transform: 'rotate(330)' }
+          }
+        }, joint.shapes.basic.Generic.prototype.defaults),
+        getPortAttrs: function (portName, index, total, selector, type) {
+          var attrs = {};
+          var portClass = 'port' + index;
+          var portSelector = selector + '>.' + portClass;
+          var portLabelSelector = portSelector + '>.port-label';
+          var portBodySelector = portSelector + '>.port-body';
+          attrs[portLabelSelector] = { text: toUI(portName) };
+          attrs[portBodySelector] = { port: { id: portName || _.uniqueId(type), type: type } };
+          attrs[portSelector] = { ref: '.body', 'ref-x': (index + 0.5) * (1 / total) };
+          if (selector === '.outPorts') {
+            attrs[portSelector]['ref-dy'] = 0;
+          }
+          return attrs;
+        }
+      }));
+
+      // Create a new environment (see createGraph function definition)
+      var env = createGraph(parentDiv, 400, 600);
+
+      // Create a cell for each node
+      for (var n in nodes) {
+        var name = toUI(n);
+        var caps = Object.keys(nodes[n].caps);
+        var reqs = Object.keys(nodes[n].reqs);
+
+        // Compute ad-hoc size for
+        var nWidth = 20 + Math.max(
+          Math.max(caps.length, reqs.length) * 20,
+          name.length * 10);
+        var nHeight = 50;
+
+        // Create cells[n], and add it to env.graph
+        cells[n] = new TopologyNode({
+          size: { width: nWidth, height: nHeight },
+          inPorts: caps,
+          outPorts: reqs,
+          attrs: { '.label': { text: name } }
+        }).addTo(env.graph);
+
+        // Replicate "type" information in "cell" to simplify double-click handling
+        cells[n].nodeType = nodes[n].type
+      }
+
+      // Create connections among node cells according to the parsed topology
+      for (var nodeId in nodes) {
+        for (var req in nodes[nodeId].reqs) {
+          var source = cells[nodeId];
+          var sourcePort = req;
+          var targetPort = topology.data.binding[req];
+          var target = cells[topology.data.capNodeId[targetPort]];
+          new joint.shapes.devs.Link({
+            source: { id: source.id, selector: source.getPortSelector(sourcePort) },
+            target: { id: target.id, selector: target.getPortSelector(targetPort) },
+            smooth: true,
+            attrs: { '.marker-target': { d: 'M 10 0 L 0 5 L 10 10 z' } }
+          }).addTo(env.graph).reparent();
+        }
+      }
+
+      // Layout the graph as a DAG
+      joint.layout.DirectedGraph.layout(env.graph, {
+        marginX: 50,
+        marginY: 50,
+        rankDir: "TB" });
+    }
+    drawTopology($("#app-topology")[0], this.state.uiData);
+  }
+});
+
+visualiser = ReactDOM.render(<Visualiser />, document.getElementById('visualiser'));
--- Barrel.js
+++ Barrel.js
@@ -1,6 +1,6 @@
 var editor = null;
 var simulator = null;
-var nodeTable = null;
+var visualiser = null;
 
 var mProt = null;
 var csar = null;
@@ -66,114 +66,6 @@ function createGraph(parentElement, width, height) {
 }
 
 /*
- * This function draws the current application's topology.
- * Inputs:
- * - "parentDiv" -> HTMLElement where to draw the application topology
- * - "topology" -> TOSCAAnalysis.UIData<Analysis.Application> representing the application topology
- */
-function drawTopology(parentDiv, topology) {
-    // Clean the content of the "parentDiv"
-    parentDiv.innerHTML = ""
-
-    // Parse the application topology
-    var nodes = topology.data.nodes;
-    var cells = {};
-
-    var toUI = function (s) { return topology.uiNames[s] || s; };
-
-    var TopologyNode = joint.shapes.basic.Generic.extend(_.extend({}, joint.shapes.basic.PortsModelInterface, {
-        markup: '<g class="rotatable"><g class="scalable"><rect class="body"/></g><text class="label"/><g class="inPorts"/><g class="outPorts"/></g>',
-        portMarkup: '<g class="port port<%= id %>"><circle class="port-body"/><text class="port-label"/></g>',
-        defaults: joint.util.deepSupplement({
-            type: 'devs.Model',
-            size: { width: 1, height: 1 },
-            inPorts: [],
-            outPorts: [],
-            attrs: {
-                '.': { magnet: false },
-                '.body': { width: 150, height: 50, 'rx': 10, 'ry': 5 },
-                '.port-body': { r: 8, magnet: 'passive' },
-                text: { 'pointer-events': 'none' },
-                '.label': { text: 'Model', 'ref-x': .5, 'ref-y': 18, ref: '.body', 'text-anchor': 'middle' },
-                '.inPorts .port-label': { x: 4, y: -9, 'text-anchor': 'start', transform: 'rotate(330)' },
-                '.outPorts .port-label': { x: 1, y: 19, 'text-anchor': 'end', transform: 'rotate(330)' }
-            }
-        }, joint.shapes.basic.Generic.prototype.defaults),
-        getPortAttrs: function (portName, index, total, selector, type) {
-            var attrs = {};
-            var portClass = 'port' + index;
-            var portSelector = selector + '>.' + portClass;
-            var portLabelSelector = portSelector + '>.port-label';
-            var portBodySelector = portSelector + '>.port-body';
-            attrs[portLabelSelector] = { text: toUI(portName) };
-            attrs[portBodySelector] = { port: { id: portName || _.uniqueId(type), type: type } };
-            attrs[portSelector] = { ref: '.body', 'ref-x': (index + 0.5) * (1 / total) };
-            if (selector === '.outPorts') {
-                attrs[portSelector]['ref-dy'] = 0;
-            }
-            return attrs;
-        }
-    }));
-
-    // Create a new environment (see createGraph function definition)
-    var env = createGraph(parentDiv, 400, 600);
-
-    // Create a cell for each node
-    for (var n in nodes) {
-        var name = toUI(n);
-        var caps = Object.keys(nodes[n].caps);
-        var reqs = Object.keys(nodes[n].reqs);
-
-        // Compute ad-hoc size for
-        var nWidth = 20 + Math.max(
-            Math.max(caps.length, reqs.length) * 20,
-            name.length * 10);
-        var nHeight = 50;
-
-        // Create cells[n], and add it to env.graph
-        cells[n] = new TopologyNode({
-            size: { width: nWidth, height: nHeight },
-            inPorts: caps,
-            outPorts: reqs,
-            attrs: { '.label': { text: name } }
-        }).addTo(env.graph);
-
-        // Replicate "type" information in "cell" to simplify double-click handling
-        cells[n].nodeType = nodes[n].type
-    }
-
-    // Create connections among node cells according to the parsed topology
-    for (var nodeId in nodes) {
-        for (var req in nodes[nodeId].reqs) {
-            var source = cells[nodeId];
-            var sourcePort = req;
-            var targetPort = topology.data.binding[req];
-            var target = cells[topology.data.capNodeId[targetPort]];
-
-            new joint.shapes.devs.Link({
-                source: { id: source.id, selector: source.getPortSelector(sourcePort) },
-                target: { id: target.id, selector: target.getPortSelector(targetPort) },
-                smooth: true,
-                attrs: { '.marker-target': { d: 'M 10 0 L 0 5 L 10 10 z' } }
-            }).addTo(env.graph).reparent();
-        }
-    }
-
-    // Attaching handler (exploiting the above map)
-    env.paper.on('cell:pointerdblclick', function (cellView, evt, x, y) {
-        var nType = cellView.model.nodeType;
-        if (nType) nodeTypeSelectorCallback(nType);
-    });
-
-    // Layout the graph as a DAG
-    joint.layout.DirectedGraph.layout(env.graph, {
-        nodeSep: 50,
-        edgeSep: 80,
-        rankDir: "TB"
-    });
-}
-
-/*
  * This function is a wrapper for "Simulator.build" and permits (re)building the "Simulator" pane from scratch.
  */
 function buildSimulator() {
@@ -198,10 +90,7 @@ var onCsarRead = function () {
     var appName = "Unnamed";
     if (serviceTemplate.hasAttribute("name"))
         appName = serviceTemplate.getAttribute("name");
-    $("#application-name")[0].innerHTML = appName;
-    //$("#topology-table-body")[0].innerHTML = TOSCAAnalysis.uiApplicationToElement(topology);
-    nodeTable.setUIData(topology);
-    drawTopology($("#app-topology")[0], topology);
+    visualiser.update(appName,topology);
 
     // !-------------------------!
     // !         EDITOR          !
