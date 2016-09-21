c21db1178c5725315a1539fd9fef1891c0cc8184
Jacopo Soldani <jsoldani@gmail.com>
2016-09-21 17:17:41 +0200

    fixed minor issue in visualising plans

--- Barrel-analyser.js
+++ Barrel-analyser.js
@@ -61,7 +61,7 @@ var PlannerResult = React.createClass({
   render: function() {
     var renderStep = step => {
       if (step.isOp) return (
-        <p key={"step-"+step.opId}>
+        <p key={"step-"+step.nodeId+"-"+step.opId}
           <span className="label label-primary">Operation</span>
           {" Execute operation "}
           <b>{this.props.getUIName(step.opId)}</b>
